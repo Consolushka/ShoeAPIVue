d6afdf52e47abcbeaca66f2d67e442e5926dda54
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2021-10-28 23:43:05 +0300

    added test MailSender.cs

---
+++ Middleware/MailSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Middleware
+{
+    public class MailSender
+    {
+        private MailAddress _from = new MailAddress("vlad@kerby.ru", "Андроид ShoeApiVue");
+        private MailAddress _to { get; set; }
+        private SmtpClient _smtpClient = new SmtpClient("smtp.yandex.ru", 465);
+
+        public MailSender(string toAddress)
+        {
+            _to = new MailAddress(toAddress);
+            _smtpClient.Credentials = new NetworkCredential("vlad@kerby.ru", "x>6^aR&8RMY:Mpc3vYFP#ot%");
+            _smtpClient.EnableSsl = true;
+        }
+
+        public void ConfirmRegistration()
+        {
+            MailMessage mailMessage = new MailMessage(_from, _to);
+            mailMessage.Subject = "Подтверждение регистрации";
+            mailMessage.Body = "<h1>Вам пришло письмо</h2>";
+            mailMessage.IsBodyHtml = true;
+            _smtpClient.Send(mailMessage);
+        }
+    }
+}
--- ShoeApi/Controllers/UserController.cs
+++ ShoeApi/Controllers/UserController.cs
@@ -35,6 +35,7 @@ namespace WebApplication.Controllers
 
             if (response)
             {
+                // new MailSender(userModel.Email).ConfirmRegistration();
                 return Ok();
             }
             
