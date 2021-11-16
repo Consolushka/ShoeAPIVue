be64d7af60b3d096dde758bbe6d10d3d62072d70
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2021-11-16 13:36:48 +0300

    added navigation prop for brands

--- Entities/Models/Brand.cs
+++ Entities/Models/Brand.cs
@@ -5,5 +5,8 @@ namespace Entities.Models
     public class Brand: Base
     {
         public string Name { get; set; }
+        
+        //Nav Prop
+        public List<Shoe> Shoes { get; set; }
     }
 }
